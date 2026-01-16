72fa3981b768f04a6df9fc28db15970f0ab65778
Manjunaath S <Manjunathsmurthy@gmail.com>
2026-01-16 13:43:00 +0530

    Add AWS VPC module variables with configurable defaults
    Added variables for VPC configuration including CIDR blocks, environment, and common tags.

---
+++ aws/modules/vpc/variables.tf
@@ -0,0 +1,35 @@
+# VPC Configuration Variables
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+}
+
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "common_tags" {
+  description = "Common tags to be applied to all resources"
+  type        = map(string)
+  default = {
+    Terraform   = "true"
+    CreatedBy   = "Terraform"
+    ManagedBy   = "Infrastructure-as-Code"
+  }
+}
